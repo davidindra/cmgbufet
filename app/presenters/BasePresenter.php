ae02492a60f90bc4f6405ef6e1ba6e68a75eed14
David Indra <mail@davidindra.cz>
2017-01-05 21:59:54 +0100

    Menu pages highlighting

--- app/presenters/BasePresenter.php
+++ app/presenters/BasePresenter.php
@@ -11,5 +11,10 @@ use App\Model;
  */
 abstract class BasePresenter extends Nette\Application\UI\Presenter
 {
+    public function beforeRender()
+    {
+        $this->redrawControl('pageNameJS');
 
+        parent::beforeRender();
+    }
 }
---
+++ app/presenters/OfferPresenter.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Presenters;
+
+use Nette;
+
+class OfferPresenter extends BasePresenter
+{
+	public function renderDefault()
+	{
+
+	}
+
+	public function actionEdit()
+	{
+		
+	}
+	
+	public function handleRemove()
+	{
+	    
+	}
+}
