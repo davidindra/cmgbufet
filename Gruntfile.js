7aea59f878cab43020c9a6ba88c2e377df31e399
David Indra <mail@davidindra.cz>
2017-01-07 23:47:37 +0100

    Grunt - use grunt-sass instead of grunt-contrib-sass (C++ implementation is faster)

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,103 +1,105 @@
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
-  // project configuration
-  grunt.initConfig({
+    require('load-grunt-tasks')(grunt);
 
-    // metadata
-    pkg: grunt.file.readJSON('package.json'),
-    banner: '/*! <%= pkg.name %> - ' +
-      '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
-      '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
-      '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;\n',
+    // project configuration
+    grunt.initConfig({
 
-    // task configuration here
+        // metadata
+        pkg: grunt.file.readJSON('package.json'),
+        banner: '/*! <%= pkg.name %> - ' +
+        '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+        '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
+        '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;\n',
 
-    watch: {
-      CSS: {
-        files: 'www/css/src/*.sass',
-        tasks: ['sass']
-      },
-      JS: {
-        files: 'www/js/src/*.js',
-        tasks: ['uglify']
-      }
-    },
+        // task configuration here
 
-    uglify: {
-      options: {
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n',
-        sourceMap: true
-      },
-      build: {
-        expand: true,
-        flatten: true,
-        src: ['www/js/src/*.js'],
-        dest: 'www/js/dist/',
-        ext: '.min.js'
-      }
-    },
-
-    sass: {
-      build: {
-        options: {
-          //style: 'expanded'
-          style: 'compressed'
+        watch: {
+            CSS: {
+                files: 'www/css/src/*.sass',
+                tasks: ['sass']
+            },
+            JS: {
+                files: 'www/js/src/*.js',
+                tasks: ['uglify']
+            }
         },
-        files: [{
-          expand: true,
-          flatten: true,
-          src: ['www/css/src/main.sass'],
-          dest: 'www/css/dist/',
-          ext: '.min.css'
-        }]
-      }
-    },
 
-    copy: {
-      dependencies: {
-        files: [
-          {
-            expand: true,
-            flatten: true,
-            src: [
-              'bower_components/jquery/dist/jquery.min.js',
-              'bower_components/jquery/dist/jquery.min.map',
-              'bower_components/materialize/dist/js/materialize.min.js',
-              'bower_components/nette.ajax.js/nette.ajax.js',
-              'bower_components/nette.ajax.js/extensions/fidRemove.ajax.js',
-              'bower_components/nette.ajax.js/extensions/spinner.ajax.js',
-              'vendor/vojtech-dobes/nette-ajax-history/client-side/history.ajax.js'
-            ],
-            dest: 'www/js/ext'
-          },
-          {
-            expand: true,
-            flatten: true,
-            src: [
-              'bower_components/materialize/dist/css/*.css'
-            ],
-            dest: 'www/css/ext'
-          },
-          {
-            expand: true,
-            flatten: true,
-            src: [
-              'bower_components/materialize/dist/fonts/roboto/*'
-            ],
-            dest: 'www/css/fonts/roboto'
-          }
-        ],
-      },
-    }
+        uglify: {
+            options: {
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n',
+                sourceMap: true
+            },
+            build: {
+                expand: true,
+                flatten: true,
+                src: ['www/js/src/*.js'],
+                dest: 'www/js/dist/',
+                ext: '.min.js'
+            }
+        },
 
-  });
+        sass: {
+            build: {
+                options: {
+                    outputStyle: 'compressed',
+                    sourceMap: true
+                },
+                files: [{
+                    expand: true,
+                    flatten: true,
+                    src: ['www/css/src/main.sass'],
+                    dest: 'www/css/dist/',
+                    ext: '.min.css'
+                }]
+            }
+        },
 
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-copy');
-  grunt.loadNpmTasks('grunt-contrib-sass');
+        copy: {
+            build: {
+                files: [
+                    {
+                        expand: true,
+                        flatten: true,
+                        src: [
+                            'bower_components/jquery/dist/jquery.min.js',
+                            'bower_components/jquery/dist/jquery.min.map',
+                            'bower_components/materialize/dist/js/materialize.min.js',
+                            'bower_components/nette.ajax.js/nette.ajax.js',
+                            'bower_components/nette.ajax.js/extensions/fidRemove.ajax.js',
+                            'bower_components/nette.ajax.js/extensions/spinner.ajax.js',
+                            'vendor/vojtech-dobes/nette-ajax-history/client-side/history.ajax.js'
+                        ],
+                        dest: 'www/js/ext'
+                    },
+                    {
+                        expand: true,
+                        flatten: true,
+                        src: [
+                            'bower_components/materialize/dist/css/*.css'
+                        ],
+                        dest: 'www/css/ext'
+                    },
+                    {
+                        expand: true,
+                        flatten: true,
+                        src: [
+                            'bower_components/materialize/dist/fonts/roboto/*'
+                        ],
+                        dest: 'www/css/fonts/roboto'
+                    }
+                ],
+            },
+        }
 
-  // default task
-  grunt.registerTask('default', ['uglify', 'sass', 'copy']);
+    });
+
+    //grunt.loadNpmTasks('grunt-contrib-uglify');
+    //grunt.loadNpmTasks('grunt-contrib-watch');
+    //grunt.loadNpmTasks('grunt-contrib-copy');
+    //grunt.loadNpmTasks('grunt-sass');
+
+    // default task
+    grunt.registerTask('default', ['uglify', 'sass', 'copy']);
 
 };
