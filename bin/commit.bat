6c84a969dc4e2d3f0a882d77a907b3017292c521
David Indra <mail@davidindra.cz>
2017-01-08 01:19:28 +0100

    Fixed Windows macro

--- bin/commit.bat
+++ bin/commit.bat
@@ -1,9 +1,10 @@
-git status;
-echo "";
-echo "Run only on master branch! Confirm with Enter.";
-pause;
-git commit -a;
-git checkout live;
-git merge master;
-git checkout master;
-git push --all;
+@echo off
+git status
+echo
+echo Run only on master branch! Confirm with Enter.
+pause
+git commit -a
+git checkout live
+git merge master
+git checkout master
+git push --all
